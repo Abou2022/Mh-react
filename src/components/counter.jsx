cf8f87400a270d5bcf69435afe6c7524cddf358f
Bakary Sylla <syllabakary2002@gmail.com>
2022-07-07 11:11:26 -0700

    Setting attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count:2
+        count:0,
+        //to get random image
+        // imageUrl: 'https://picsum.photos/200',
+    };
+
+    styles = {
+        fontSize: 50,
+        fontWeight: 'bold'
     };
 
     render() { 
         return ( 
         <div>
-            <span>{this.formatCount}</span>
-            <button>Increment</button>
+            {/* <img src={this.state.imageUrl} alt="" /> */}
+            <span style={this.styles} className='badge badge-primary m-2'>{this.formatCount()}</span>
+            <button className='btn btn-secondary btn-sm'>Increment</button>
         </div>
         );
     }
